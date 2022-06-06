696d7e105d36642d4bdad18d1be3532094ea6262
Taylor Jones <tjonesster@gmail.com>
2022-06-06 14:58:25 -0500

    deleted some files

--- test9.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-export TF_FORCE_UNIFIED_MEMORY=1
-export XLA_PYTHON_CLIENT_MEM_FRACTION=4.0
-
-
-
-./run_alphafold.py --fasta_names forAlican11.fasta --job_name forAlican22_gpu --model_names model_5_multimer_v2 --overwrite --num_structures 1 --num_recycle 4 --use_precomputed_msas --model_preset multimer
