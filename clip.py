49c3e04d0fd350e940635079422383686006c54e
Taylor Jones <tjonesster@gmail.com>
2022-06-07 14:06:18 -0500

    relocated some additional files

--- clip.py
+++
@@ -1,8 +0,0 @@
-lines = []
-with open("old_file_list.txt",'r') as f:
-    for line in f:
-        lines.append(line.split("/")[-1])
-
-with open('filelist.txt', 'w') as f:
-    for line in lines:
-        f.write(line)
--- junk/drd4.sh
+++ junk/drd4.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./run_alphafold.py --fasta_names DRD4_AF_A.fasta --job_name DRD4.fasta --model_names model_5 --overwrite --num_structures 10  --use_precomputed_msas --db_preset reduced_dbs
+./run_alphafold.py --fasta_names DRD4_AF_A.fasta --job_name DRD4.fasta_10_recycles --model_names model_5 --overwrite --num_structures 10  --use_precomputed_msas --db_preset reduced_dbs --num_recycle 10
