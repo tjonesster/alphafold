624173394f025cbb228c276fd4a379cdf11f8925
Taylor Jones <tjonesster@gmail.com>
2022-06-07 13:15:16 -0500

    added the model_config dict to the arguments.txt file

--- otc1_2.sh
+++ otc1_2.sh
@@ -4,5 +4,5 @@ export options2=' --num_recycle 0 '
 
 for i in {1..100}
 do
-./run_alphafold.py --fasta_names hOTC1.fasta --job_name asdfasdff$i --model_names model_5 --overwrite --num_structures 1  --use_precomputed_msas --db_preset reduced_dbs $options $options2 --norun_relax
+./run_alphafold.py --fasta_names hOTC1.fasta --job_name asdfasdfff$i --model_names model_5 --overwrite --num_structures 1  --use_precomputed_msas --db_preset reduced_dbs $options $options2 --norun_relax
 done
