179af6c1663bcd38ba28f9e2364f1fbbdf895928
Taylor Jones <tjonesster@gmail.com>
2022-06-11 02:09:20 -0500

    hardened off the submission scripts

--- alphafold_job_submitter.sh
+++ alphafold_job_submitter.sh
@@ -7,9 +7,9 @@ source /home/jonestl5/miniconda3/bin/activate af2_7
 #alphafold_runner.slurm
 while read p; do 
 echo $p 
-sbid=$(sbatch --export=infile=$p,db_preset=reduced_dbs alphafold_runner_cache.slurm) 
-sbid2=$(sbatch --export=infile=$p,db_preset=full_dbs alphafold_runner_cache.slurm) 
+sbid=$(sbatch --export=infile=$p,db_preset=reduced_dbs alphafold_runner_cache.slurm | awk '{print $4}') 
+sbid2=$(sbatch --export=infile=$p,db_preset=full_dbs alphafold_runner_cache.slurm | awk '{print $4}' ) 
 
-sbatch --dependency=$sbid,$sbid2 --exporrt=infile=$p alphafold_runner.slurm
+sbatch --dependency=afterok:$sbid,afterok:$sbid2 --export=infile=$p alphafold_runner.slurm
 
 done < fileList.txt
